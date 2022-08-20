ed33842ea695270ee5a3a51fdef47f5960a89105
arza1012 <arzabala12@gmail.com>
2022-08-20 07:51:51 -0500

    new tab lab testing added

--- scripts/main.js
+++ scripts/main.js
@@ -8,27 +8,3 @@ myImage.onclick = () => {
     myImage.setAttribute('src','images/salud-digital.jpeg');
   }
 }
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-    const myName = prompt('Escribe tu nombre:');
-    if (!myName) {
-        setUserName();
-      } else {
-        localStorage.setItem('name', myName);
-        myHeading.textContent = `Sé el especialista de tu salud, ${myName}`;
-      }
-    }
-
-  if (!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem('name');
-    myHeading.textContent = `Sé el especialista de tu salud, ${storedName}`;
-  }
-
-  myButton.onclick = () => {
-    setUserName();
-  }
